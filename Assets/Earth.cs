c8ad79a3df75beeace7a006ceeca14bde8162772
Henry Yao <henryyao1126@gmail.com>
2018-10-06 20:34:58 +0800

    W3 Homework v1.0

---
+++ Assets/Earth.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Earth : MonoBehaviour {
+
+    public float y;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Transform transform = GetComponent<Transform>();
+        Vector3 angle = new Vector3(0, y + Time.deltaTime, 0);
+        transform.Rotate(angle);
+    }
+}
